f15f12c8fd4e517c0615d30c6c4a6dfc680f3d8f
cel017 <psidharth20@gmail.com>
2025-12-23 21:22:55 -0700

    test

--- llvm/lib/CodeGen/RegAllocSSA.cpp
+++ llvm/lib/CodeGen/RegAllocSSA.cpp
@@ -1,11 +1,14 @@
 #include "RegAllocBase.h"
 #include "AllocationOrder.h"
+#include "llvm/ADT/STLExtras.h"
 #include "llvm/CodeGen/LiveIntervals.h"
+#include "llvm/CodeGen/LiveRangeEdit.h"
 #include "llvm/CodeGen/LiveRegMatrix.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineLoopInfo.h"
 #include "llvm/CodeGen/Passes.h"
 #include "llvm/CodeGen/RegAllocRegistry.h"
+#include "llvm/CodeGen/Spiller.h"
 #include "llvm/CodeGen/VirtRegMap.h"
 #include "llvm/CodeGen/MachineRegisterInfo.h"
 #include "llvm/CodeGen/TargetRegisterInfo.h"
@@ -27,13 +30,15 @@ class RASSA : public MachineFunctionPass, public RegAllocBase {
   MachineFunction *MF = nullptr;
   MachineLoopInfo *MLI = nullptr;
 
-  // Needed to satisfy RegAllocBase interface, but we use manual spilling
-  class DummySpiller : public Spiller {
+  // We define a Manual Spiller that does nothing because we handle 
+  // stack assignment manually in selectOrSplit to bypass the 
+  // crashing 'isRematerializable' logic in standard LLVM spillers.
+  class ManualSSA_Spiller : public Spiller {
   public:
     void spill(LiveRangeEdit &) override {}
     void postOptimization() override {}
   };
-  DummySpiller DS;
+  ManualSSA_Spiller MSSASpiller;
 
 public:
   static char ID;
@@ -47,111 +52,115 @@ public:
     AU.addRequired<VirtRegMapWrapperLegacy>();
     AU.addRequired<LiveRegMatrixWrapperLegacy>();
     AU.addRequired<MachineLoopInfoWrapperPass>();
+    
+    // Preserve analyses for the Deconstruction pass and the Rewriter
     AU.addPreserved<LiveIntervalsWrapperPass>();
     AU.addPreserved<VirtRegMapWrapperLegacy>();
     AU.addPreserved<SlotIndexesWrapperPass>();
     MachineFunctionPass::getAnalysisUsage(AU);
   }
 
-  // --- Fernando's Weight Method ---
-  // w = (1 + 10^depth) [for def] + sum(1 + 10^depth) [for each use]
+  /// Fernando's Weighting Algorithm:
+  /// w(v) = (1 + 10^loop_depth(def)) + sum_{uses}(1 + 10^loop_depth(use))
   float calculateFernandoWeight(Register Reg) {
     float Weight = 0.0f;
     MachineRegisterInfo &MRI = MF->getRegInfo();
 
     auto AddWeight = [&](const MachineInstr *MI) {
+      if (!MI) return;
       unsigned Depth = MLI->getLoopDepth(MI->getParent());
       Weight += (1.0f + std::pow(10.0f, (float)Depth));
     };
 
-    if (MachineInstr *DefMI = MRI.getVRegDef(Reg))
-      AddWeight(DefMI);
-
+    AddWeight(MRI.getVRegDef(Reg));
     for (MachineInstr &UseMI : MRI.use_instructions(Reg))
       AddWeight(&UseMI);
 
     return Weight;
   }
 
-  Spiller &spiller() override { return DS; }
+  Spiller &spiller() override { return MSSASpiller; }
 
   void enqueueImpl(const LiveInterval *LI) override {}
   const LiveInterval *dequeue() override { return nullptr; }
 
   MCRegister selectOrSplit(const LiveInterval &VirtReg,
                            SmallVectorImpl<Register> &SplitVRegs) override {
-    // 1. Try to find free physical register
+    
+    // 1. Try to assign a physical register
     auto Order = AllocationOrder::create(VirtReg.reg(), *VRM, RegClassInfo, Matrix);
     for (MCRegister PhysReg : Order) {
       if (Matrix->checkInterference(VirtReg, PhysReg) == LiveRegMatrix::IK_Free)
         return PhysReg;
     }
 
-    // 2. Manual Spill (Fernando Method)
-    // If it's a PHI or critical, we must not spill. 
-    // Otherwise, we assign a stack slot and the rewriter handles it.
+    // 2. If no PhysReg is free, we "Spill" by assigning a stack slot manually.
+    // PHIs and ImplicitDefs are protected (marked unspillable) in the main loop.
     if (!VirtReg.isSpillable())
       return 0;
 
-    if (!VRM->hasStackSlot(VirtReg.reg()))
+    if (VRM->getStackSlot(VirtReg.reg()) == VirtRegMap::NO_STACK_SLOT)
       VRM->assignVirt2StackSlot(VirtReg.reg());
 
-    return 0; 
+    return 0; // Return 0 to indicate stack residency
   }
 
   bool runOnMachineFunction(MachineFunction &mf) override {
     MF = &mf;
     MLI = &getAnalysis<MachineLoopInfoWrapperPass>().getLI();
     auto &LIS = getAnalysis<LiveIntervalsWrapperPass>().getLIS();
-    auto &VRM = getAnalysis<VirtRegMapWrapperLegacy>().getVRM();
-    auto &Matrix = getAnalysis<LiveRegMatrixWrapperLegacy>().getLRM();
+    auto &VRM_Wrapper = getAnalysis<VirtRegMapWrapperLegacy>().getVRM();
+    auto &LRM = getAnalysis<LiveRegMatrixWrapperLegacy>().getLRM();
     MachineRegisterInfo &MRI = MF->getRegInfo();
 
-    RegAllocBase::init(VRM, LIS, Matrix);
+    RegAllocBase::init(VRM_Wrapper, LIS, LRM);
     RegClassInfo.runOnMachineFunction(*MF);
 
-    // --- Prepare Weights & Protect PHIs ---
     std::vector<Register> VRegs;
+
+    // --- Phase 1: Pre-process and Weighting ---
     for (unsigned i = 0, e = MRI.getNumVirtRegs(); i != e; ++i) {
       Register Reg = Register::index2VirtReg(i);
       if (MRI.reg_nodbg_empty(Reg)) continue;
 
       LiveInterval &LI = LIS.getInterval(Reg);
+      
+      // Prioritize registers in deep loops using Fernando's algorithm
       LI.setWeight(calculateFernandoWeight(Reg));
 
-      // PHI Protection: Mark unspillable to prevent Spiller crashes
-      if (MRI.getVRegDef(Reg) && MRI.getVRegDef(Reg)->isPHI()) {
+      // Shield PHIs and ImplicitDefs from spilling logic to prevent Segfaults
+      MachineInstr *DefMI = MRI.getVRegDef(Reg);
+      if (DefMI && (DefMI->isPHI() || DefMI->isImplicitDef())) {
         LI.markNotSpillable();
         LI.setWeight(HUGE_VALF); 
       }
       VRegs.push_back(Reg);
     }
 
-    // --- Sort by Fernando's Weights ---
+    // --- Phase 2: Priority Sorting ---
     std::sort(VRegs.begin(), VRegs.end(), [&](Register A, Register B) {
       LiveInterval &LIA = LIS.getInterval(A);
       LiveInterval &LIB = LIS.getInterval(B);
       if (LIA.isSpillable() != LIB.isSpillable())
-        return !LIA.isSpillable();
+        return !LIA.isSpillable(); 
       return LIA.weight() > LIB.weight();
     });
 
-    // --- Main Allocation Loop ---
+    // --- Phase 3: Assignment ---
     for (Register Reg : VRegs) {
-      if (VRM.hasPhys(Reg)) continue;
+      if (VRM->hasPhys(Reg)) continue;
       LiveInterval &LI = LIS.getInterval(Reg);
       
       SmallVector<Register, 4> SplitVRegs;
       MCRegister PhysReg = selectOrSplit(LI, SplitVRegs);
       
       if (PhysReg) {
-        Matrix.assign(LI, PhysReg);
+        Matrix->assign(LI, PhysReg);
       } else if (!LI.isSpillable()) {
-        report_fatal_error("Could not allocate register for PHI node in SSA!");
+        report_fatal_error("Infeasible: SSA PHI node could not be allocated!");
       }
     }
 
-    mf.getProperties().set(MachineFunctionProperties::Property::NoPHIs);
     return true;
   }
 };
@@ -160,11 +169,13 @@ char RASSA::ID = 0;
 
 } // end anonymous namespace
 
-INITIALIZE_PASS_BEGIN(RASSA, "ssa", "SSA Allocator", false, false)
+INITIALIZE_PASS_BEGIN(RASSA, "ssa", "SSA Chordal-Style Allocator", false, false)
 INITIALIZE_PASS_DEPENDENCY(LiveIntervalsWrapperPass)
 INITIALIZE_PASS_DEPENDENCY(VirtRegMapWrapperLegacy)
 INITIALIZE_PASS_DEPENDENCY(LiveRegMatrixWrapperLegacy)
 INITIALIZE_PASS_DEPENDENCY(MachineLoopInfoWrapperPass)
-INITIALIZE_PASS_END(RASSA, "ssa", "SSA Allocator", false, false)
+INITIALIZE_PASS_END(RASSA, "ssa", "SSA Chordal-Style Allocator", false, false)
 
-FunctionPass *llvm::createSSARegisterAllocator() { return new RASSA(); }
+namespace llvm {
+  FunctionPass *createSSARegisterAllocator() { return new RASSA(); }
+}
